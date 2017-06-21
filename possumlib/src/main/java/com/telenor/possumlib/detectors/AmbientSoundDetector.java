ec67a336f2581915b5d9d40ab91548a4e1eb1a3f
Brynje Nordli <bno@teleplan.no>
2017-06-21 10:43:00 +0200

    Sound now posts correct features to rest api

--- possumlib/src/main/java/com/telenor/possumlib/detectors/AmbientSoundDetector.java
+++ possumlib/src/main/java/com/telenor/possumlib/detectors/AmbientSoundDetector.java
@@ -18,8 +18,6 @@ import com.telenor.possumlib.constants.DetectorType;
 import com.telenor.possumlib.models.PossumBus;
 import com.telenor.possumlib.utils.sound.SoundFeatureExtractor;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -31,13 +29,10 @@ public class AmbientSoundDetector extends AbstractDetector {
     private AudioManager audioManager;
     private AudioRecord audioRecorder;
     private Handler audioHandler;
-    private SoundFeatureExtractor mfcc;
     private final int bufferSize;
-    private final int windowSamples;
     private final int recordingSamples;
     private boolean disabledMute;
     private boolean supportsUnprocessed;
-    private List<double[]> features = new ArrayList<>();
     private ExecutorService backgroundService = Executors.newSingleThreadExecutor();
 
     /**
@@ -50,11 +45,10 @@ public class AmbientSoundDetector extends AbstractDetector {
      */
     public AmbientSoundDetector(Context context, String identification, @NonNull PossumBus eventBus, boolean authenticating) {
         super(context, identification, eventBus, authenticating);
-        windowSamples = sampleRate() * windowSize() / 1000;
+//        int windowSamples = sampleRate() * windowSize() / 1000;
         recordingSamples = sampleRate() * ((int) listenInterval() / 1000);
         bufferSize = AudioTrack.getMinBufferSize(sampleRate(), AudioFormat.CHANNEL_OUT_MONO, audioEncoding());
-        mfcc = new SoundFeatureExtractor();
-        features = new ArrayList<>();
+//        SoundFeatureExtractor mfcc = new SoundFeatureExtractor();
         audioHandler = getAudioHandler();
         audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         audioRecorder = getAudioRecord();
@@ -148,29 +142,19 @@ public class AmbientSoundDetector extends AbstractDetector {
     private class RecordThread implements Runnable {
         @Override
         public void run() {
-            Log.i(tag, "Starting to read from stream");
+            Log.i(tag, "Starting to read from audio stream");
             short[] buffer = new short[bufferSize];
             int recordedSamples = 0;
             int readSize;
             while (isListening() && isRecording() && recordedSamples < recordingSamples) {
                 if ((readSize = audioRecorder.read(buffer, 0, bufferSize)) != AudioRecord.ERROR_INVALID_OPERATION) {
-                        // Calculate features
-//                        Log.d(tag, "Calculating features from "+readSize+" samples");
-//                        Log.d(tag, "Corresponding to "+(float)readSize/sampleRate()*1000+ " milliseconds");
-                        List<double []> buffer_features = SoundFeatureExtractor.getFeaturesFromSample(
-                                buffer, readSize, sampleRate());
-//                        Log.d(tag, "Got "+buffer_features.size()+" time windows from buffer");
-                        features.addAll(buffer_features);
-                        recordedSamples += readSize;
-//                    System.arraycopy(buffer, 0, data, recordedSamples, readSize); // old
-//                    recordedSamples += readSize; // old
+                    // Calculate features
+                    for (double[] window : SoundFeatureExtractor.getFeaturesFromSample(buffer, readSize, sampleRate())) {
+                        sessionValues().add(SoundFeatureExtractor.writeFeatureWindowToJsonArray(window));
+                    }
+                    recordedSamples += readSize;
                 }
             }
-            // Write sound features to json array and add to session values
-            sessionValues().add(SoundFeatureExtractor.writeFeaturesToJsonArray(features));
-            stopRecording();
-            //SoundFeatureExtractor.writeFeaturesToFile(features, storedData());
-            features.clear();
         }
     }
 
@@ -207,7 +191,7 @@ public class AmbientSoundDetector extends AbstractDetector {
      * @return the recording length in milliseconds
      */
     public long listenInterval() {
-        return 5000;
+        return 3000;
     }
 
     private void stopRecording() {
--- possumlib/src/main/java/com/telenor/possumlib/utils/sound/SoundFeatureExtractor.java
+++ possumlib/src/main/java/com/telenor/possumlib/utils/sound/SoundFeatureExtractor.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import com.google.gson.JsonArray;
 
+import org.joda.time.DateTime;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,10 +22,11 @@ public class SoundFeatureExtractor {
     /**
      * Takes a sound sample, divides it into partially overlapping windows, and returns an array of
      * sound features for every window.
+     *
      * @param samples       Audio sample in the PCM 16 bit format
      * @param sample_size   Size of the audio sample
      * @param sampling_rate Sampling rate (Hz)
-     * @return  List of audio samples of length windows. Each item in the list is an array of
+     * @return List of audio samples of length windows. Each item in the list is an array of
      * of length number_of_features.
      */
     public static List<double[]> getFeaturesFromSample(short[] samples, int sample_size,
@@ -32,60 +35,59 @@ public class SoundFeatureExtractor {
         // Convert source to double and absolute values (for SoundFeatureExtractor)
         double[] samples_double_abs = new double[sample_size];
         double[] samples_double = new double[sample_size];
-        for (int i=0; i<sample_size; i++) {
+        for (int i = 0; i < sample_size; i++) {
             samples_double_abs[i] = Math.abs((double) samples[i]);
             samples_double[i] = (double) samples[i];
         }
         List<double[]> features = new ArrayList<>();
-        long time = System.currentTimeMillis();
+        long time = DateTime.now().getMillis();
         int current = 0;
         // Window size hardcoded since it need to be a power of 2 for FFT
         //int window_size = 4096; // This is a window of 85 ms @ 48000 hz sample rate
         int window_size = 2048; // This is a window of 46 ms @ 44100 hz sample rate
         while (current + window_size <= sample_size) {
             try {
-                double [] window_data_abs = Arrays.copyOfRange(samples_double_abs,
-                        current, current+window_size);
-                double [] window_data = Arrays.copyOfRange(samples_double,
-                        current, current+window_size);
+                double[] window_data_abs = Arrays.copyOfRange(samples_double_abs,
+                        current, current + window_size);
+                double[] window_data = Arrays.copyOfRange(samples_double,
+                        current, current + window_size);
                 // Get SoundFeatureExtractor features
-                double [] mfcc_features = get_mfcc(window_data_abs, (double) sampling_rate);
+                double[] mfcc_features = get_mfcc(window_data_abs, (double) sampling_rate);
                 // Get LPC features
-                double [] lpc_features = get_lpc(window_data);
+                double[] lpc_features = get_lpc(window_data);
                 // Get time-domain features
                 double zcr = get_zcr(window_data);
                 double ste = get_ste(window_data);
                 // Compute FFT and get frequency domain features
                 FFT.computeFFT(window_data);
-                double [] fft_real = FFT.real;
-                double [] fft_imag = FFT.imag;
-                double [] fft = new double[fft_real.length];
+                double[] fft_real = FFT.real;
+                double[] fft_imag = FFT.imag;
+                double[] fft = new double[fft_real.length];
                 // Convert complex number to magnitude
-                for (int j=0; j<fft_real.length; j++) {
-                    fft[j] = Math.sqrt(fft_imag[j]*fft_imag[j]+fft_real[j]*fft_real[j]);
+                for (int j = 0; j < fft_real.length; j++) {
+                    fft[j] = Math.sqrt(fft_imag[j] * fft_imag[j] + fft_real[j] * fft_real[j]);
                 }
                 // Get frequency domain features
                 double sc = get_sc(fft);
                 double peak = get_peak(fft);
                 double sf = get_flatness(fft);
-                double [] tmp = new double [number_of_features];
+                double[] tmp = new double[number_of_features];
                 // Make time the first column
                 tmp[0] = time;
                 // Copy in features to result array
                 System.arraycopy(mfcc_features, 0, tmp, 1, FeatureExtractor.numCepstra);
-                tmp[FeatureExtractor.numCepstra+1] = zcr;
-                tmp[FeatureExtractor.numCepstra+2] = ste;
-                tmp[FeatureExtractor.numCepstra+3] = sc;
-                tmp[FeatureExtractor.numCepstra+4] = sf;
-                tmp[FeatureExtractor.numCepstra+5] = peak;
-                System.arraycopy(lpc_features, 0, tmp, FeatureExtractor.numCepstra+6, lpc_dimensions);
+                tmp[FeatureExtractor.numCepstra + 1] = zcr;
+                tmp[FeatureExtractor.numCepstra + 2] = ste;
+                tmp[FeatureExtractor.numCepstra + 3] = sc;
+                tmp[FeatureExtractor.numCepstra + 4] = sf;
+                tmp[FeatureExtractor.numCepstra + 5] = peak;
+                System.arraycopy(lpc_features, 0, tmp, FeatureExtractor.numCepstra + 6, lpc_dimensions);
                 // Add to list of features
                 features.add(tmp);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            current += window_size*(1-window_overlap);
+            current += window_size * (1 - window_overlap);
         }
         return features;
     }
@@ -93,14 +95,15 @@ public class SoundFeatureExtractor {
     /**
      * Writes the supplied list of features to a space separated ASCII file in the External Storage
      * Directory with each item on the list on its own line.
-     * @param feature_list  List of features
-     * @param outputFile    The file to output to
+     *
+     * @param feature_list List of features
+     * @param outputFile   The file to output to
      */
     public static void writeFeaturesToFile(List<double[]> feature_list, File outputFile) {
         FileOutputStream feature_file = null;
         try {
             feature_file = new FileOutputStream(outputFile);
-            for (double [] features: feature_list) {
+            for (double[] features : feature_list) {
                 String feature_string = "";
                 for (double feature : features) {
                     feature_string += " " + feature;
@@ -110,13 +113,13 @@ public class SoundFeatureExtractor {
             }
             feature_file.flush();
         } catch (Exception e) {
-            Log.e(tag, "Failed to write to file:",e);
+            Log.e(tag, "Failed to write to file:", e);
         } finally {
             if (feature_file != null) {
                 try {
                     feature_file.close();
                 } catch (IOException e) {
-                    Log.e(tag, "Failed to close file:",e);
+                    Log.e(tag, "Failed to close file:", e);
                 }
             }
         }
@@ -125,23 +128,22 @@ public class SoundFeatureExtractor {
     /**
      * Returns a json array of json arrays (number of time windows, number of features). Casts all
      * numbers to string to avoid rounding.
+     *
      * @param feature_list List of features
-     * @return  json array of json arrays containing features
+     * @return json array of json arrays containing features
      */
-    public static JsonArray writeFeaturesToJsonArray(List<double[]> feature_list) {
-        JsonArray feature_list_json = new JsonArray();
-        for (double [] features: feature_list) {
-            JsonArray features_array = new JsonArray();
-            for (int i=0; i<number_of_features; i++) {
-                features_array.add(Double.toString(features[i]));
-            }
-            feature_list_json.add(features_array);
+    public static JsonArray writeFeatureWindowToJsonArray(double[] feature_list) {
+        JsonArray array = new JsonArray();
+        for (int i = 0; i < number_of_features; i++) {
+            if (i == 0) array.add(Long.toString((long) feature_list[i]));
+            else array.add(Double.toString(feature_list[i]));
         }
-        return feature_list_json;
+        return array;
     }
 
     /**
      * Returns the MFCC features of a supplied audio sample
+     *
      * @param samples       The audio signal for the sample
      * @param sampling_rate The sampling rate in Hz
      * @return MFCC coefficients
@@ -156,8 +158,9 @@ public class SoundFeatureExtractor {
 
     /**
      * Returns the Linear Prediction Cepstral coefficients
-     * @param samples       The audio signal for the sampele
-     * @return  LPC coefficients (lenght lpc_dimensions)
+     *
+     * @param samples The audio signal for the sampele
+     * @return LPC coefficients (lenght lpc_dimensions)
      * @throws Exception
      */
     private static double[] get_lpc(double[] samples) throws Exception {
@@ -244,54 +247,58 @@ public class SoundFeatureExtractor {
 
     /**
      * Returns the Zero Crossing Rate of a supplied audio sample
-     * @param samples       The audio signal for the sample
-     * @return  Zero Crossing Rate
+     *
+     * @param samples The audio signal for the sample
+     * @return Zero Crossing Rate
      */
-    private static double get_zcr(double [] samples) {
+    private static double get_zcr(double[] samples) {
         double zcr = 0.0;
-        for (int i=1; i<samples.length; i++) {
-            zcr += Math.abs(Math.signum(samples[i]) - Math.signum(samples[i-1]));
+        for (int i = 1; i < samples.length; i++) {
+            zcr += Math.abs(Math.signum(samples[i]) - Math.signum(samples[i - 1]));
         }
         return zcr / samples.length;
     }
 
     /**
      * Returns the Short-time Energy of a supplied audio sample
-     * @param samples       The audio signal for the sample
-     * @return  Short-time Energy
+     *
+     * @param samples The audio signal for the sample
+     * @return Short-time Energy
      */
-    private static double get_ste(double [] samples) {
+    private static double get_ste(double[] samples) {
         double ste = 0.0;
-        for (int i=0; i<samples.length; i++) {
-            ste += samples[i]*samples[i];
+        for (int i = 0; i < samples.length; i++) {
+            ste += samples[i] * samples[i];
         }
         return ste / samples.length;
     }
 
     /**
      * Returns the Spectral Centroid of the supplied audio sample
-     * @param spectrum  The Fourier spectrum of the audio sample
-     * @return  Spectral Centroid
+     *
+     * @param spectrum The Fourier spectrum of the audio sample
+     * @return Spectral Centroid
      */
-    private static double get_sc(double [] spectrum) {
+    private static double get_sc(double[] spectrum) {
         double num = 0.0;
         double den = 0.0;
-        for (int i=0; i<spectrum.length; i++) {
-            num += i*spectrum[i];
+        for (int i = 0; i < spectrum.length; i++) {
+            num += i * spectrum[i];
             den += spectrum[i];
         }
-        return num/den;
+        return num / den;
     }
 
     /**
      * Returns the spectral peak, i.e. the frequency with the largest amplitude
-     * @param spectrum  The Fourier spectrum of the audio sample
-     * @return  Spectral Peak
+     *
+     * @param spectrum The Fourier spectrum of the audio sample
+     * @return Spectral Peak
      */
-    private static double get_peak(double [] spectrum) {
+    private static double get_peak(double[] spectrum) {
         double max = 0.0;
         int peak = 0;
-        for (int i=0; i<spectrum.length; i++) {
+        for (int i = 0; i < spectrum.length; i++) {
             if (spectrum[i] > max) {
                 peak = i;
                 max = spectrum[i];
@@ -302,17 +309,18 @@ public class SoundFeatureExtractor {
 
     /**
      * Returns the spectral flatness of a supplied audio sample
-     * @param spectrum  The Fourier spectrum of the audio sample
-     * @return  Spectral Flatness
+     *
+     * @param spectrum The Fourier spectrum of the audio sample
+     * @return Spectral Flatness
      */
-    private static double get_flatness(double [] spectrum) {
+    private static double get_flatness(double[] spectrum) {
         double sum = 0.0;
         double sumln = 0.0;
-        double denom = 1.0/spectrum.length;
-        for (int i=0; i<spectrum.length; i++) {
+        double denom = 1.0 / spectrum.length;
+        for (int i = 0; i < spectrum.length; i++) {
             sum += spectrum[i];
             sumln += Math.log(spectrum[i]);
         }
-        return Math.exp(denom*sumln)/denom/sum;
+        return Math.exp(denom * sumln) / denom / sum;
     }
 }
